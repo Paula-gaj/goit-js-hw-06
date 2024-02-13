2a86efd384b84136931efc6e8c89990daadb7539
Paula-gaj <paula-sk@wp.pl>
2024-02-13 18:30:20 +0100

    finished

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
 const fontSizeControl = document.getElementById("font-size-control");
 const text = document.getElementById("text");
 
+document.getElementById("text").style.fontSize = "56px";
+
 fontSizeControl.addEventListener("input", () => {
   const fontSize = fontSizeControl.value + "px";
   text.style.fontSize = fontSize;
--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,36 @@
+const BASE_WIDTH_PX = 30;
+const BASE_HEIGHT_PX = BASE_WIDTH_PX;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const container = document.querySelector("div#boxes");
+const input = document.querySelector("div#controls > input");
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    const elem = document.createElement("div");
+    elem.style.width = `${BASE_WIDTH_PX + 10 * i}px`;
+    elem.style.height = `${BASE_HEIGHT_PX + 10 * i}px`;
+    elem.style.backgroundColor = getRandomHexColor();
+    container.insertAdjacentElement("beforeend", elem);
+  }
+}
+
+function destroyBoxes() {
+  container.innerHTML = "";
+}
+
+function handleCreateClick() {
+  const boxes = input.value;
+  createBoxes(boxes);
+}
+
+const createButton = document.querySelector("button[data-create]");
+const destroyButton = document.querySelector("button[data-destroy]");
+
+createButton.addEventListener("click", handleCreateClick);
+destroyButton.addEventListener("click", destroyBoxes);
